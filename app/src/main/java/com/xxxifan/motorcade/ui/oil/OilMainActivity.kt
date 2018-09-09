672625d92c87df56756f70a277ac9e3c6cd522ff
xxxifan <445947962@qq.com>
2018-09-09 22:47:37 +0800

    Oil相关布局

---
+++ app/src/main/java/com/xxxifan/motorcade/ui/oil/OilMainActivity.kt
@@ -0,0 +1,24 @@
+package com.xxxifan.motorcade.ui.oil
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import org.jetbrains.anko.button
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
+
+class OilMainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    verticalLayout {
+      button("记录油耗") {
+        setOnClickListener { startActivity<OilRecordActivity>() }
+      }
+      button("查看记录") {
+        setOnClickListener { toast("再努力一点，就能看到了哦") }
+      }
+      gravity = Gravity.CENTER
+    }
+  }
+}
